b848d77cbc382af99f0de3cffb43bb43c77db80f
Arcxcad <deadman123543@gmail.com>
2026-05-29 20:49:30 -0300

    Script para el albun
    Script para cerrar la imagen por la x o tocando espacio vacio

---
+++ Wiki/script.js
@@ -0,0 +1,29 @@
+function openModal(imgElement) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const caption = document.getElementById("modal-caption");
+
+  modal.style.display = "flex";
+  modal.classList.add("show");
+  modalImg.src = imgElement.src;
+  caption.innerText = imgElement.alt;
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("show");
+  setTimeout(() => {
+    modal.style.display = "none";
+    document.getElementById("modal-caption").innerText = "";
+  }, 300);
+}
+
+// Cerrar modal si se hace clic en el fondo oscuro
+document.getElementById("modal").addEventListener("click", function(e) {
+  const modalImg = document.getElementById("modal-img");
+  const closeBtn = document.querySelector(".close");
+
+  if (e.target === this) {
+    closeModal();
+  }
+});
